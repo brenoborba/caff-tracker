5a054e0e2dc15432f8e5d4cf63b38b5fa994dbaf
brenoborba <brenoborba99@gmail.com>
2022-03-22 11:15:20 +0100

    max-height added

--- app.js
+++ app.js
@@ -2,51 +2,45 @@ const coffee = document.querySelector('.coffee')
 const oneMug = document.getElementById('one-mug')
 const twoMug = document.getElementById('two-mug')
 const threeMug = document.getElementById('three-mug')
-const caffLimit = 94
+const caffLimit = 280
 let caff1 = 56
 let caff2 = 28
 let caff3 =  18.6
+let displayUpdater
+
 
 function changeHeight(qtd){
+  let newHeight
   let height = getComputedStyle(coffee).getPropertyValue('height')
   let convertedHeight = parseInt(height)
-  let newHeight
   newHeight = convertedHeight + qtd
   coffee.style.setProperty('height', newHeight + "px")
-}
-
-oneMug.onclick = function(){
-  changeHeight(caff1)
-}
-
-twoMug.onclick = function(){
-  changeHeight(caff2)
-}
-
-threeMug.onclick = function(){
-  changeHeight(caff3)
+  displayUpdater = newHeight
 }
 
 
+function btnFunc(btn, caff){
+  btn.onclick = function(){
+    changeHeight(caff)
+  }
+}
 
-
-
-
-
-/* function move(caff) {
-  if (i == 0) {
-    i = 1;
-    var elem = document.querySelector('.coffee')
-    var height = 1;
-    var id = setInterval(frame, 10);
-    function frame() {
-        if (height >= caff) {
-          clearInterval(id);
-          i = 0;
-        } else {
-          height++;
-          elem.style.height = height + "%";
-        }
-      }
+/* if(displayUpdater <= caffLimit){
+  oneMug.onclick = function(){
+    changeHeight(caff1)
+  }
+  
+  twoMug.onclick = function(){
+    changeHeight(caff2)
+  }
+  
+  threeMug.onclick = function(){
+    changeHeight(caff3)
   }
 } */
+
+
+
+btnFunc(oneMug, caff1)
+btnFunc(twoMug, caff2)
+btnFunc(threeMug, caff3)
