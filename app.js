453495975887bb5e6b7b6cdd15e3a59961c136e5
brenoborba <brenoborba99@gmail.com>
2022-03-24 17:30:12 +0100

    Warning message for when the caffeine limit is reached added

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ const oneMug = document.getElementById('one-mug')
 const twoMug = document.getElementById('two-mug')
 const threeMug = document.getElementById('three-mug')
 const clearBtn = document.getElementById('clear')
+const message = document.querySelector('#message')
 const caffLimit = 280
 let caff1 = 56
 let caff2 = 28
 let caff3 =  18.6
-let displayUpdater
+let heightMonitor
+
+message.style.display = 'none'
 
 
 function clearHeight(btn){
@@ -22,7 +25,8 @@ function increaseHeight(qtd){
   let convertedHeight = parseInt(height)
   newHeight = convertedHeight + qtd
   coffee.style.setProperty('height', newHeight + "px")
-  displayUpdater = newHeight
+  heightMonitor = newHeight
+  showWarningMessage()
 }
 
 
@@ -32,10 +36,19 @@ function btnFunc(btn, caff){
   }
 }
 
-btnFunc(oneMug, caff1)
-btnFunc(twoMug, caff2)
-btnFunc(threeMug, caff3)
+function showWarningMessage(){
+  if (heightMonitor >= caffLimit){
+    message.style.display = 'block'
+  }
+}
 
-clearHeight(clearBtn)
+function main(){
+  btnFunc(oneMug, caff1)
+  btnFunc(twoMug, caff2)
+  btnFunc(threeMug, caff3)
+  clearHeight(clearBtn)
 
+  
+}
 
+main()
